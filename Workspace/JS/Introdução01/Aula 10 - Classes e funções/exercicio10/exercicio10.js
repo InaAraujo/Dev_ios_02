30fd3c1bb67f46f3aa4b14a005c001d4ab3c1866
InaAraujo <inaiaraaraujo25@hotmail.com>
2022-10-28 21:25:40 -0300

    Aula 28/10

---
+++ Workspace/JS/Introdução01/Aula 10 - Classes e funções/exercicio10/exercicio10.js
@@ -0,0 +1,8 @@
+class carro{
+    constructor(id, nome, idade, desistente ){
+        this.nome = id;
+        this.ano = nome;
+        this.porta = idade;
+        this.janelas = desistente;
+
+    }
